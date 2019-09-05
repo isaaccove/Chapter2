32015b72ea1bcb109b90bb2d4a4aa620705eb444
ISAAC COVE <isaaccove@gmail.com>
2019-09-05 15:45:32 +0100

    My fourth Commit

--- isaac2.java
+++ isaac2.java
@@ -1,11 +1,14 @@
 public class Isaac2{
-	
+
+
 public static void main(String[] args){
 
 
 	System.out.println("Welcome to my World");
-	System.out.printf("I am Invinsible");
-	System.out.print("I am Born to rule my WORLD");
+
+	System.out.println("I am Invinsible");
+
+	System.out.println("I am Born to rule my WORLD");
 
 }
 
